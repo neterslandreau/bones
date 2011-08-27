ab8629b199ed23dec1acf0a87e544f9bfc739ba4
Niles <neterslandreau@gmail.com>
2011-08-26 22:18:57 -0400

    overriding the users edit function

--- app/controllers/app_users_controller.php
+++ app/controllers/app_users_controller.php
@@ -7,6 +7,7 @@ class AppUsersController extends UsersController {
 	public function beforeFilter() {
 		parent::beforeFilter();
 		$this->User = ClassRegistry::init('AppUser');
+		$this->User->Detail = ClassRegistry::init('AppDetail');
 	}
 
 	public function render($action = null, $layout = null, $file = null) {
@@ -17,6 +18,33 @@ class AppUsersController extends UsersController {
 			$file = App::pluginPath('users') . 'views' . DS . 'users' . DS . $action . '.ctp';
 		}
 		return parent::render($action, $layout, $file);
+	}
+
+/**
+ * Edit
+ *
+ * @param string $id User ID
+ * @return void
+ */
+	public function edit() {
+		if (!empty($this->data)) {
+			$this->data['Detail'] = $this->data['Detail']['User'];
+			unset($this->data['Detail']['User']);
+
+			if ($this->User->Detail->saveSection($this->Auth->user('id'), $this->data, 'User')) {
+				$this->Session->setFlash(__d('users', 'Profile saved.', true));
+			} else {
+				$this->Session->setFlash(__d('users', 'Could not save your profile.', true));
+			}
+		} else {
+			$this->data = $this->User->find('first', array(
+				'conditions' => array(
+					'User.id' => $this->Auth->user('id'),
+				)
+			));
+		}
+
+		$this->_setLanguages();
 	}
 
 }
