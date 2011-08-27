9482e71c0ec8f37e6ebf8971523b614009e437e3
Niles <neterslandreau@gmail.com>
2011-08-26 22:21:35 -0400

    overriding methods in AppUser model for refactoring.

--- app/models/app_user.php
+++ app/models/app_user.php
@@ -4,4 +4,48 @@ class AppUser extends User {
     public $useTable = 'users';
     public $name = 'AppUser';
 	public $alias = 'User';
+
+/**
+ * hasMany associations
+ *
+ * @var array
+ */
+	public $hasMany = array(
+		'Detail' => array(
+			'className' => 'AppDetail',
+			'foreign_key' => 'user_id'));
+
+/**
+ * Constructor
+ *
+	public function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
+	}
+/**
+ * Registers a new user
+ *
+ * @param array $postData Post data from controller
+ * @param boolean $useEmailVerification If set to true a token will be generated
+ * @return mixed
+ */
+	public function register($postData = array(), $useEmailVerification = true) {
+		$postData = $this->_beforeRegistration($postData, $useEmailVerification);
+
+		$this->_removeExpiredRegistrations();
+
+		$this->set($postData);
+//debug($this->Detail);
+		if ($this->validates()) {
+			App::import('Core', 'Security');
+			$postData[$this->alias]['passwd'] = Security::hash($postData[$this->alias]['passwd'], 'sha1', true);
+			$this->create();
+			$ret = $this->save($postData, false);
+			$this->Detail->createDefaults($this->id);
+			return $ret;
+		}
+
+		return false;
+	}
+
+
 }
